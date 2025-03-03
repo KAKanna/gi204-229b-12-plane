37d25f71c2f4434c68a3bc053d19208a4c2fc1ff
KAKanna <mlg666pop@gmail.com>
2025-03-03 16:52:56 +0700

    Well-done plane can fly

--- Assets/Script/Air Plane.cs
+++ Assets/Script/Air Plane.cs
@@ -7,6 +7,9 @@ public class AirPlane : MonoBehaviour
     [SerializeField] float liftBooster = 0.5f;
     [SerializeField] float drag = 0.01f;
     [SerializeField] float angularDrag = 0.01f;
+    [SerializeField] float yawPower = 50f;
+    [SerializeField] float pitchPower = 50f;
+    [SerializeField] float rollPower = 30f;
 
     private void Start()
     {
@@ -18,5 +21,18 @@ public class AirPlane : MonoBehaviour
         {
             rb.AddForce(transform.forward * enginePower);
         }
+
+        Vector3 lift = Vector3.Project(rb.linearVelocity,transform.forward);
+        rb.AddForce(transform.up * lift.magnitude * liftBooster);
+        rb.linearDamping = rb.linearVelocity.magnitude * drag;
+        rb.angularDamping = rb.linearVelocity.magnitude * angularDrag;
+
+        float yaw = Input.GetAxis("Horizontal") * yawPower;
+        float pitch = Input.GetAxis("Vertical") * pitchPower;
+        float roll = Input.GetAxis("Roll") * rollPower;
+
+        rb.AddTorque(transform.up*yaw);
+        rb.AddTorque(transform.right*pitch);
+        rb.AddTorque(transform.forward*roll);
     }
 }
